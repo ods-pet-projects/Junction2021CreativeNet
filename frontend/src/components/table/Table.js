3dea9dc6e53af66239f20b7d2b733f613ee5a18a
Yevhenii Bezeha <bezegajenja0305@gmail.com>
2021-11-20 20:25:32 +0200

    add build, sort

--- frontend/src/components/table/Table.js
+++ frontend/src/components/table/Table.js
@@ -2,13 +2,13 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Cards from "../cards/Cards";
 import style from "./Table.module.css";
-import { ClassNames } from "@emotion/react";
 
 const TableComponent = () => {
   const [data, setData] = useState([]);
   const [showCards, setShowCards] = useState(false);
   const [el, setEl] = useState({});
-  const [onRow, setOnRow] = useState(false);
+  const [onSort, setOnSort] = useState(false);
+  const [property, setProperty] = useState("");
 
   useEffect(() => {
     axios
@@ -21,12 +21,30 @@ const TableComponent = () => {
     setEl(el);
   };
 
+  useEffect(() => {
+    if (onSort) {
+      const sort = (property) => {
+        const sortedData = [...data.sort((a, b) => b[property] - a[property])];
+        setData(sortedData);
+      };
+      sort(property);
+      return;
+    }
+
+    if (!onSort) {
+      const sort = (property) => {
+        const sortedData = [...data.sort((a, b) => a[property] - b[property])];
+        setData(sortedData);
+      };
+      sort(property);
+    }
+  }, [onSort]);
+
   const showTable = () => setShowCards(false);
 
-  const sort = (property) => {
-    const sortedData = data.sort((a, b) => b[property] - a[property]);
-    console.log(sortedData);
-    setData(sortedData);
+  const sort = (string) => {
+    setOnSort(!onSort);
+    setProperty(string);
   };
 
   return (
@@ -36,15 +54,21 @@ const TableComponent = () => {
       ) : (
         <table class="rwd-table">
           <tr>
-            <th className={style.main}>ID</th>
+            <th>ID</th>
             <th className={style.main} onClick={() => sort("probability")}>
               Probability
             </th>
-            <th className={style.main}>Sensor quality</th>
-            <th className={style.main}>Loading</th>
-            <th className={style.main}>Speed</th>
-            <th className={style.main}>Action</th>
-            <th className={style.main}>Details</th>
+            <th className={style.main} onClick={() => sort("sensor_quality")}>
+              Sensor quality
+            </th>
+            <th className={style.main} onClick={() => sort("loading")}>
+              Loading
+            </th>
+            <th className={style.main} onClick={() => sort("speed")}>
+              Speed
+            </th>
+            <th>Action</th>
+            <th>Details</th>
           </tr>
           {data.map((el) => {
             return (
