834c225bfd5ff70b25577c6ce3b4d357e23be240
submaps <amironsoft@mail.ru>
2021-11-21 05:56:27 +0300

    add shap values and explanation to api and model

--- backend/app.py
+++ backend/app.py
@@ -10,18 +10,46 @@ if os.path.exists('/home/junc/mlcup/kone'):
 else:
     kone_dir = '/Users/o/PycharmProjects/Junction2021CreativeNet/kone_dir'
 
-
 app = Flask(__name__)
 CORS(app)
 
 
+def dataframe_to_json(df):
+    lines = []
+    for i, row in df.iterrows():
+        lines.append(dict(row))
+    return lines
+
+
 @app.route("/api/get_queries")
 def get_queries():
-    with open(f"{kone_dir}/samples_100.json") as ifile:
-        lines = json.load(ifile)
-    # df_pred = pd.read_csv(f'{kone_dir}/test_predicted_add.csv')
-    # return jsonify(df_pred.head(100).to_json())
-    return jsonify(lines)
+    # with open(f"{kone_dir}/samples_100.json") as ifile:
+    #     lines = json.load(ifile)
+    # return jsonify(lines)
+    #
+    # "id": "ele0030053",
+    # "probability": "0.8883406824917287",
+    # "action_type": "art01",
+    # "sensor_quality": "0.6817229639907736",
+    # "loading": "6",
+    # "area_id": "ga00000073",
+    # "speed": "7"
+
+    df_pred = pd.read_csv(f'{kone_dir}/train_predicted_add_300.csv')
+    df_pred['shap_vals_plot'] = df_pred['shap_vals_plot'].apply(json.loads)
+    cols = ['equipment_id',
+            'probability',
+            'action_recommendation_type',
+            'sensor_quality',
+            'load_category',
+            'equipment_area',
+            'speed_category',
+            'shap_vals_plot',
+            'shap_inter_str']
+
+    df_pred = df_pred[cols]
+    rows = dataframe_to_json(df_pred)
+    return jsonify(rows)
 
 
 if __name__ == "__main__":
--- models.py
+++ models.py
@@ -2,6 +2,7 @@ import numpy as np
 import os
 import pandas as pd
 from catboost import CatBoostClassifier
+import shap
 
 
 if os.path.exists('/home/junc/mlcup/kone'):
@@ -59,20 +60,104 @@ def add_sensor_quality(test):
     return test
 
 
-def main():
+def get_last_model():
+    cat_cols = ['case_id', 'equipment_id', 'action_recommendation_id', 'action_recommendation_type',
+                'action_recommendation_category', 'equipment_area', 'usage_type', 'equipment_category']
+
+    model = CatBoostClassifier(cat_features=cat_cols, iterations=600)
+    model.load_model(f'{kone_dir}/catboost_model')
+    return model
+
+
+def save_test_predict_df():
     test = pd.read_csv(f'{kone_dir}/test.csv')
     test = add_features(test)
+
+    test = prepare_X(test)
+
+    model = get_last_model()
+    y_pred = model.predict_proba(test)[:, 1]
+    test['probability'] = y_pred
+    test = add_sensor_quality(test)
+    test.to_csv(f'{kone_dir}/test_predicted_add.csv', index=False)
+
+
+def shap_inter(shap_row):
+    interpret = []
+    str_equip_area = "Feature equipment_area defined the geographically close equipments shows that the visit is "
+    str_recom_action_id = "Unique identifier of the service action recommendation action_recomendation_id shows that the visit is "
+    str_usage_type = "Feature related to equipment belonging to the same usage_type shows that the visit is "
+
+    if shap_row.iloc[5].value >= 0:
+        interpret.append(str_equip_area + "necessary.")
+    else:
+        interpret.append(str_equip_area + "unnecessary.")
+
+    if shap_row.iloc[6].value >= 0:
+        interpret.append(str_usage_type + "necessary.")
+    else:
+        interpret.append(str_usage_type + "unnecessary.")
+
+    if shap_row.iloc[2].value >= 0:
+        interpret.append(str_recom_action_id + "necessary.")
+    else:
+        interpret.append(str_recom_action_id + "unnecessary.")
+    return "\n ".join(interpret)
+
+
+def add_shap_info(train):
+    model = get_last_model()
+    explainer = shap.Explainer(model)
+    shap_values = explainer(train[model.feature_names_].fillna(''))
+
+    shap_vals_plot_list = []
+    shap_inter_str_list = []
+
+    for i in range(len(shap_values)):
+        shap_row = shap_values[i]
+        shap_row_df = pd.DataFrame({'value': shap_row.values,
+                                   'data': shap_row.data,
+                                   'shap_value': [shap_row.base_values] * len(shap_values[1].values),
+                                   'cols': model.feature_names_
+                                   })
+        shap_row_df['values_abs'] = shap_row_df['value'].abs()
+        shap_vals_plot = shap_row_df.sort_values('values_abs', ascending=False).head(3)
+        # shap_vals_plot.index = shap_vals_plot['cols']
+        shap_vals_plot['color'] = np.where(shap_vals_plot['value'] < 0, 'blue', 'red')
+        shap_vals_plot_list.append(shap_vals_plot.to_json())
+        # shap_vals_plot['value'].plot(kind='bar', xlabel='cols', color=shap_vals_plot['color'].values)
+        shap_inter_str = shap_inter(shap_row_df)
+        shap_inter_str_list.append(shap_inter_str)
+
+    train['shap_vals_plot'] = shap_vals_plot_list
+    train['shap_inter_str'] = shap_inter_str_list
+
+    # visualize the first prediction's explanation
+    # shap.plots.waterfall(shap_values[0])
+    return train
+
+
+def save_train_predict_df():
+    train = pd.read_csv(f'{kone_dir}/train.csv')
+    train = add_features(train)
 
     cat_cols = ['case_id', 'equipment_id', 'action_recommendation_id', 'action_recommendation_type',
                 'action_recommendation_category', 'equipment_area', 'usage_type', 'equipment_category']
 
     model = CatBoostClassifier(cat_features=cat_cols, iterations=600)
     model.load_model(f'{kone_dir}/catboost_model')
-    test = prepare_X(test)
-    y_pred = model.predict_proba(test)[:, 1]
-    test['probability'] = y_pred
-    test = add_sensor_quality(test)
-    test.to_csv(f'{kone_dir}/test_predicted_add.csv', index=False)
+    train = prepare_X(train)
+    y_pred = model.predict_proba(train)[:, 1]
+    train['probability'] = y_pred
+    train = add_sensor_quality(train)
+    train = train.head(300)
+    train = add_shap_info(train)
+    train.to_csv(f'{kone_dir}/train_predicted_add_300.csv', index=False)
+
+
+def main():
+    # save_test_predict_df()
+    save_train_predict_df()
 
 
 if __name__ == '__main__':
